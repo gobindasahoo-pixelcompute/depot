53d283c20633172b5c7de252ac607cc41b59d412
Gobinda Sahoo <gobinda.sahoo@pixelcompute.com>
2025-02-22 17:41:42 +0530

    add cart scaffold

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,6 @@
 class StoreController < ApplicationController
   def index
+    @products = Product.order(:title) 
+    # to get the products in order of their title 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_20_034236) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_22_120607) do
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
