4c73edc6adc75ab5fc8e414792136396ddfa7770
Gobinda Sahoo <gobinda.sahoo@pixelcompute.com>
2025-02-23 08:53:13 +0530

    add smarter cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -23,11 +23,11 @@ class LineItemsController < ApplicationController
 
   # POST /line_items or /line_items.json
   def create
-     product = Product.find(params[:product_id])
-     @line_item = @cart.line_items.build(product: product)
+    product = Product.find(params[:product_id])
+     @line_item = @cart.add_product(product)
     respond_to do |format|
     if @line_item.save
-     format.html { redirect_to cart_url(@line_item.cart),
+    format.html { redirect_to cart_url(@line_item.cart),
     notice: "Line item was successfully created." }
     format.json { render :show,
     status: :created, location: @line_item }
@@ -38,7 +38,7 @@ class LineItemsController < ApplicationController
     status: :unprocessable_entity }
     end
     end
-    end
+  end
 
   # PATCH/PUT /line_items/1 or /line_items/1.json
   def update
