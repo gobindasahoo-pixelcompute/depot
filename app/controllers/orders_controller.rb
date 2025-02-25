9b967e9e0fc91f6c2c1e797fcce26b242f275817
Gobinda Sahoo <gobinda.sahoo@pixelcompute.com>
2025-02-25 23:17:58 +0530

    completed Iteration H1

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -26,20 +26,20 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
     @order.add_line_items_from_cart(@cart)
-
     respond_to do |format|
-      if @order.save
-        Cart.destroy(session[:cart_id])
-        session[:cart_id] = nil
-        format.html { redirect_to store_index_url, notice: 
-          'Thank you for your order.' }
-        format.json { render :show, status: :created,
-          location: @order }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @order.errors,
-          status: :unprocessable_entity }
-      end
+    if @order.save
+    Cart.destroy(session[:cart_id])
+    session[:cart_id] = nil
+     OrderMailer.received(@order).deliver_later
+    format.html { redirect_to store_index_url, notice:
+    'Thank you for your order.' }
+    format.json { render :show, status: :created,
+    location: @order }
+    else
+    format.html { render :new, status: :unprocessable_entity }
+    format.json { render json: @order.errors,
+    status: :unprocessable_entity }
+    end
     end
   end
 
