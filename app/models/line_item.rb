0f6c65dd256873f990c8dc532c88ec6e1c7b4f6d
Gobinda Sahoo <gobinda.sahoo@pixelcompute.com>
2025-02-25 07:28:44 +0530

    completed Iteration G1

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,7 @@
 class LineItem < ApplicationRecord
-  belongs_to :product
-  belongs_to :cart
+  belongs_to :order, optional: true 
+  belongs_to :product 
+  belongs_to :cart, optional: true 
 
   def total_price
     (product.price || 1) * quantity.to_i 
